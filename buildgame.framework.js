ffe925851d95b3b6f0e99600479ef5f68467e3c0
김민지 <holomiso0617@gmail.com>
2025-07-17 08:43:05 +0900

    Add Unity WebGL build files with LFS

---
+++ buildgame.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:adc3cd21d96a6eace6ad9f5ef342346b73cb45f6c43369af340cb6256903835d
+size 444608
---
+++ buildgame.loader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:003ddf7e0e06ba91d126ed7e0b55fd202fc385344e78ba03c81ffebe20fb686c
+size 26454
